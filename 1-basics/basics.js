bd070e8753f2a4bf88ed33be6c146cabeea5dcf1
Marjoriemuloma <marjoriemuloma1@gmail.com>
2019-10-25 14:02:26 +0300

    example test

--- 1-basics/basics.js
+++ 1-basics/basics.js
@@ -12,7 +12,7 @@ describe("basics", function() {
         it("should run without errors", function() {
             ////////////////////////////////////////////////////////////////////
             // ✏️ ADD CODE HERE ✏️
-
+            const someVariable = true;
             ////////////////////////////////////////////////////////////////////
 
             expect(someVariable).to.equal(true);
