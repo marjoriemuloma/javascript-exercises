563ccd200aa328942b6279b49c4999e94dba0c33
Marjoriemuloma <marjoriemuloma1@gmail.com>
2019-10-26 20:13:01 +0300

    deleting a property from an object

--- 1-basics/basics.js
+++ 1-basics/basics.js
@@ -172,7 +172,7 @@ describe("basics", function() {
 
             ////////////////////////////////////////////////////////////////////
             // ✏️ ADD CODE HERE ✏️
-
+            delete object.someProperty;
             ////////////////////////////////////////////////////////////////////
 
             // object.someProperty is undefined...
