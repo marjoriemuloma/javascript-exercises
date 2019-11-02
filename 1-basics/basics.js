2a27bd8e3b908ceae46b9df8b640907325488533
Marjoriemuloma <marjoriemuloma1@gmail.com>
2019-11-02 18:58:16 +0300

    a falsy object

--- 1-basics/basics.js
+++ 1-basics/basics.js
@@ -314,7 +314,7 @@ describe("basics", function() {
             ////////////////////////////////////////////////////////////////////
             // Can you find a value that is typeof "object" but also casts to `false`?
             // ✏️ ADD CODE HERE ✏️ 
-            propertyName = "secondProperty";
+            const falsyThing = null;
 
 
             ////////////////////////////////////////////////////////////////////
