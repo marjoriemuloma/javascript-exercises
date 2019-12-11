cf9093183c5112cb3fd3190eb45fdc5c56c75935
Marjoriemuloma <marjoriemuloma1@gmail.com>
2019-12-11 20:42:37 +0300

    reverse the order of the array

--- 4-standard-lib/arrays.js
+++ 4-standard-lib/arrays.js
@@ -159,7 +159,9 @@ describe("arrays", function() {
 
             ////////////////////////////////////////////////////////////////////
             // ✏️ ADD CODE HERE. USE THE STANDARD LIB, NO LOOPS! ✏️
-
+            function backwards(){
+                return array.reverse();
+            }
             ////////////////////////////////////////////////////////////////////
 
             backwards();
